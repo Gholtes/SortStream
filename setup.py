7b2c3fb9fa8e38efee1da4f9918aefbeaf3be791
Grant Holtes <gwholtes@gmail.com>
2020-10-24 10:55:57 +1100

    pypi 1.2.1

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='SortStream',
-    version='1.2.0',
+	name='SortStream',
+	version='1.2.1',
 	author = "Grant Holtes",
 	author_email = "gwholes@gmail.com",
 	url = "https://github.com/Gholtes/SortStream",
+	download_url="https://github.com/Gholtes/SortStream/archive/v1.2.tar.gz",
 	keywords = ["nlp", "classification", "document", "pdf"],
-    packages=['sortstream'],
+	packages=['sortstream'],
 	install_requires=[
 		"nltk",
 		"numpy",
 		"PyPDF2",
 		"sklearn"
 	],
-    license="MIT",
+	license="MIT",
 	description='NLP Document Classification Tool.',
-    long_description=open('README.txt').read()
+	long_description_content_type='text/markdown',
+	long_description=open('pypiReadme.md', 'r').read()
 )
